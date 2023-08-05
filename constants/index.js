edee18d152fe1bcab6d1926eb1c62af26f42a80e
FarzadMoh <124052629+FarzadMoh@users.noreply.github.com>
2023-08-05 15:15:19 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x2c30d991948bb087758a434bb6efff2a3c20789e";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
